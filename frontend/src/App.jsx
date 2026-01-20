79ef6c1547c78314bdd0ce07d5760f50a3a51e72
williamobrienmlmhi <william.obrien@mlmhi.com>
2026-01-20 01:37:04 -0500

    Add frontend App component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>MLM Shingle Checker</h1>
+      <p>Frontend is running.</p>
+    </div>
+  );
+}
+
+export default App;
